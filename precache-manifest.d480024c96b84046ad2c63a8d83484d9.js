3c58b5df8cb7b4990e7798ba2b0c59d91ea2e680
andrewkucz <kuczynskiandrew@gmail.com>
2019-06-17 18:48:37 -0400

    Updates

---
+++ precache-manifest.d480024c96b84046ad2c63a8d83484d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e057c4bc230e2a658c1ebd4d1f15f8da",
+    "url": "/Jake-and-Amir-Ranked/index.html"
+  },
+  {
+    "revision": "4633f19c57080f2a9196",
+    "url": "/Jake-and-Amir-Ranked/static/css/main.73c944fe.chunk.css"
+  },
+  {
+    "revision": "266015aa53d5022b7fa0",
+    "url": "/Jake-and-Amir-Ranked/static/js/2.4028265d.chunk.js"
+  },
+  {
+    "revision": "4633f19c57080f2a9196",
+    "url": "/Jake-and-Amir-Ranked/static/js/main.173bc8d1.chunk.js"
+  },
+  {
+    "revision": "9a778abd597f85656ef4",
+    "url": "/Jake-and-Amir-Ranked/static/js/runtime~main.36f918d6.js"
+  }
+]);
